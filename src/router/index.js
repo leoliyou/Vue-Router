ab0815ccd83ebaf6e9c95e73e9be7ce761f90532
leoliyou <liyouleoleo@gmail.com>
2023-09-16 00:00:00 +0100

    component

--- src/router/index.js
+++ src/router/index.js
@@ -14,7 +14,7 @@ const router =  new VueRouter({
 			name:'guanyu',
 			path:'/about',
 			component:About,
-			meta:{title:'关于'}
+			meta:{isAuth:true,title:'关于'}
 		},
 		{
 			name:'zhuye',
@@ -27,8 +27,8 @@ const router =  new VueRouter({
 					path:'news',
 					component:News,
 					meta:{isAuth:true,title:'新闻'},
-					beforeEnter: (to, from, next) => {
-						console.log('独享路由守卫',to,from)
+					/* beforeEnter: (to, from, next) => {
+						console.log('前置路由守卫',to,from)
 						if(to.meta.isAuth){ //判断是否需要鉴权
 							if(localStorage.getItem('school')==='atguigu'){
 								next()
@@ -38,7 +38,7 @@ const router =  new VueRouter({
 						}else{
 							next()
 						}
-					}
+					} */
 				},
 				{
 					name:'xiaoxi',
