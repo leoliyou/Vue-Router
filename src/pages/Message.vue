f1a13d519a11a8d515a65f3495fa25acc2fadea2
leoliyou <liyouleoleo@gmail.com>
2023-09-15 23:57:32 +0100

    programming

--- src/pages/Message.vue
+++ src/pages/Message.vue
@@ -15,7 +15,8 @@
 				}">
 					{{m.title}}
 				</router-link>
-			
+				<button @click="pushShow(m)">push查看</button>
+				<button @click="replaceShow(m)">replace查看</button>
 			</li>
 		</ul>
 		<hr>
@@ -33,6 +34,26 @@
 					{id:'002',title:'消息002'},
 					{id:'003',title:'消息003'}
 				]
+			}
+		},
+		methods: {
+			pushShow(m){
+				this.$router.push({
+					name:'xiangqing',
+					query:{
+						id:m.id,
+						title:m.title
+					}
+				})
+			},
+			replaceShow(m){
+				this.$router.replace({
+					name:'xiangqing',
+					query:{
+						id:m.id,
+						title:m.title
+					}
+				})
 			}
 		},
 	}
